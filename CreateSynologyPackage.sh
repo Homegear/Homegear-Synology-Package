4d96ec3e631b952628ded6df9a6ceef6d9a7a4b3
Sathya Laufer <sathya@laufers.net>
2015-11-26 23:37:51 +0100

    Small changes

--- CreateSynologyPackage.sh
+++ CreateSynologyPackage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Run this script in a Debian Wheezy debootstrap or docker environment for all three processor types used by Synology (armhf, i386, amd64)
+# Run this script in a Debian wheezy debootstrap or docker environment for all three processor types used by Synology (armel, i386, amd64)
 
 download() {
 	wget http://homegear.eu/downloads/nightlies/$1
@@ -21,35 +21,59 @@ unzip master.zip
 rm master.zip
 cd Homegear-Synology-Package-master/Template
 
-download libhomegear-base_current_debian_wheezy_armhf.deb
+download libhomegear-base_current_debian_wheezy_armel.deb
 version=`cat control | grep Version: | cut -d " " -f 2`
 arch=`cat control | grep Architecture: | cut -d " " -f 2`
-download homegear_current_debian_wheezy_armhf.deb
-download homegear-homematicbidcos_current_debian_wheezy_armhf.deb
-download homegear-homematicwired_current_debian_wheezy_armhf.deb
-download homegear-insteon_current_debian_wheezy_armhf.deb
-download homegear-max_current_debian_wheezy_armhf.deb
-download homegear-philipshue_current_debian_wheezy_armhf.deb
-download homegear-sonos_current_debian_wheezy_armhf.deb
-
-exit 0
+download homegear_current_debian_wheezy_armel.deb
+download homegear-homematicbidcos_current_debian_wheezy_armel.deb
+download homegear-homematicwired_current_debian_wheezy_armel.deb
+download homegear-insteon_current_debian_wheezy_armel.deb
+download homegear-max_current_debian_wheezy_armel.deb
+download homegear-philipshue_current_debian_wheezy_armel.deb
+download homegear-sonos_current_debian_wheezy_armel.deb
 
 mkdir -p Package/lib/homegear
 echo "version=\"${version}\"" >> SPK/INFO
 
 cp /usr/lib/pyshared/python2.7/lzo.so Package/lib/homegear
+find /lib -name libbsd.so.0 -exec cp {} Package/lib/homegear \;
 find /lib -name libgcrypt.so.11 -exec cp {} Package/lib/homegear \;
+find /lib -name libglib-2.0.so.0 -exec cp {} Package/lib/homegear \;
 find /lib -name libgpg-error.so.0 -exec cp {} Package/lib/homegear \;
+find /lib -name liblzma.so.5 -exec cp {} Package/lib/homegear \;
+find /lib -name libpcre.so.3 -exec cp {} Package/lib/homegear \;
 find /lib -name libreadline.so.6 -exec cp {} Package/lib/homegear \;
 find /lib -name libtinfo.so.5 -exec cp {} Package/lib/homegear \;
+find /lib -name libdl.so.2 -exec cp {} Package/lib/homegear \;
+find /lib -name libpthread.so.0 -exec cp {} Package/lib/homegear \;
 find /lib -name libz.so.1 -exec cp {} Package/lib/homegear \;
+find /lib -name librt.so.1 -exec cp {} Package/lib/homegear \;
+find /lib -name libnsl.so.1 -exec cp {} Package/lib/homegear \;
+find /lib -name libcrypt.so.1 -exec cp {} Package/lib/homegear \;
+find /lib -name libm.so.6 -exec cp {} Package/lib/homegear \;
+find /lib -name libc.so.6 -exec cp {} Package/lib/homegear \;
+find /lib -name libgcc_s.so.1 -exec cp {} Package/lib/homegear \;
+find /usr/lib -iname libcrypto.so.1.0.0 -exec cp {} Package/lib/homegear \;
+find /usr/lib -iname libedit.so.2 -exec cp {} Package/lib/homegear \;
+find /usr/lib -iname libenchant.so.1 -exec cp {} Package/lib/homegear \;
+find /usr/lib -iname libexslt.so.0 -exec cp {} Package/lib/homegear \;
+find /usr/lib -iname libgmodule-2.0.so.0 -exec cp {} Package/lib/homegear \;
+find /usr/lib -iname libgmp.so.10 -exec cp {} Package/lib/homegear \;
 find /usr/lib -iname libgnutls.so.26 -exec cp {} Package/lib/homegear \;
+find /usr/lib -iname libltdl.so.7 -exec cp {} Package/lib/homegear \;
+find /usr/lib -iname libmcrypt.so.4 -exec cp {} Package/lib/homegear \;
 find /usr/lib -iname libp11-kit.so.0 -exec cp {} Package/lib/homegear \;
+find /usr/lib -iname libqdbm.so.14 -exec cp {} Package/lib/homegear \;
 find /usr/lib -iname libsqlite3.so.0 -exec cp {} Package/lib/homegear \;
-find /usr/lib -iname libstdc++.so.6 -exec cp {} Package/lib/homegear \;
+find /usr/lib -iname libssl.so.1.0.0 -exec cp {} Package/lib/homegear \;
 find /usr/lib -iname libtasn1.so.3 -exec cp {} Package/lib/homegear \;
+find /usr/lib -iname libxml2.so.2 -exec cp {} Package/lib/homegear \;
+find /usr/lib -iname libxslt.so.1 -exec cp {} Package/lib/homegear \;
+find /usr/lib -iname libstdc++.so.6 -exec cp {} Package/lib/homegear \;
 
 cp -R etc usr var Package
+
+chmod 755 SPK/scripts/*
 
 cd Package
 tar -czf package.tgz *
